c24933c1ee22048da53c9415ace3a5b8d5b018c0
Atul Dwivedi <er.dwivediatul@gmail.com>
2018-01-21 16:42:17 +0530

    jdbc-hello-world-demo

---
+++ src/com/atuldwivedi/jee/learn/jdbc/JdbcHelloWorld.java
@@ -0,0 +1,40 @@
+package com.atuldwivedi.jee.learn.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JdbcHelloWorld {
+	public static void main(String[] args) {
+		Connection con = null;
+		try {
+			// connect to the database
+			con = DriverManager.getConnection("jdbc:h2:~/test", "sa", "");
+
+			// create statement
+			Statement stmt = con.createStatement();
+
+			// execute the query
+			ResultSet rs = stmt.executeQuery("SELECT * FROM EMP");
+
+			// fetch the result
+			while (rs.next()) {
+				int empId = rs.getInt(1);
+				String empName = rs.getString(2);
+				System.out.println(empId + " " + empName);
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+}
